3ab7d70019f7a66497aed476c4552ed1a2bb6864
zeta <zezeon1@gmail.com>
2019-11-10 14:24:43 +0900

    2019. 11. 10. (일) 14:24:43 KST autocommit

--- src/plog.cpp
+++ src/plog.cpp
@@ -1,14 +1,4 @@
-#include<plog/Log.h>
-#include<plog/Appenders/ColorConsoleAppender.h>
-using namespace std;
 
 int main()
-{
-	plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
-	plog::init(plog::verbose, &consoleAppender);
-	plog::init<1>(plog::verbose, "plog.txt");
-	LOGD << "hello this is logging";
-	LOGD_(1) << "file";
-}
-
+{}
 
